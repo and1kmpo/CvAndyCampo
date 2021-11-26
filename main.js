cd686566377edded798e6c0612b89458a136423c
and1kmpo <acampolpez@gmail.com>
2021-11-26 16:18:44 -0500

    Se crea menú sticky y se modifica animación del menú al pasar mouse por enlaces, modificación num telefónico

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
+const navLi = document.querySelector(".nav-menu-item");
 
 navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("nav-menu_visible");
+  navMenu.classList.toggle("nav-menu_visible"),navMenu.classList.toggle("nav-menu-item");
 
   if (navMenu.classList.contains("nav-menu_visible")) {
     navToggle.setAttribute("aria-label", "Cerrar menú");
@@ -10,5 +11,3 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-label", "Abrir menú");
   }
 });
-
-
